80dc72453bf8127b6287dc86a46c5e7d291e5f14
Andrei <abrongel@gmail.com>
2019-08-13 16:00:34 -0300

    SDL_TTF e eventos de toque

--- Makefile
+++ Makefile
@@ -4,14 +4,14 @@ CXX_FLAGS := -w -std=c++17 -ggdb
 BIN			 := bin
 SRC			 := src/source.cpp
 
-SDL_CONFIG   := `sdl2-config --cflags --libs` -lSDL2_image
+SDL_CONFIG   := `sdl2-config --cflags --libs` -lSDL2_image -lSDL2_ttf
 
 LIBRARIES	:=
 EXECUTABLE	:= my_game
 
 
 all: $(SRC)
-	$(CXX) $(CXX_FLAGS) $(SRC) $(SDL_CONFIG) -o $(BIN)/$(EXECUTABLE)
+	$(CXX) $(CXX_FLAGS) $(SRC) $(SDL_CONFIG) $(LIBRARIES) -o $(BIN)/$(EXECUTABLE)
 
 run: clean all
 	clear
--- src/source.cpp
+++ src/source.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
 #include <vector>
+#include <unordered_map>
+
 #include <SDL.h>
 #include <SDL_image.h>
+#include <SDL_ttf.h>
+
+class Vec2 {
+public:
+    float x = 0.f;
+    float y = 0.f;
+};
 
 namespace drawing{
     SDL_Renderer* renderer;
@@ -15,18 +24,34 @@ namespace drawing{
     void present(){
         SDL_RenderPresent(renderer);
     }
+
+    const Vec2 screen_point_to_world(const Vec2& screen_point) {
+        int winW, winH;
+        SDL_GetRendererOutputSize(drawing::renderer, &winW, &winH);
+
+        Vec2 out = screen_point;
+        out.x -= winW / 2;
+        out.y = winH / 2 - out.y;
+        return out; 
+    }
+
+    const Vec2 touch_point_to_screen(const float x, const float y){
+        int winW, winH;
+        SDL_GetRendererOutputSize(drawing::renderer, &winW, &winH);
+
+        Vec2 out;
+        out.x = x * (float)winW;
+        out.y = y * (float)winH;
+
+        return out;
+    }
 }
 
-class vec2 {
+class Drawable {
 public:
-    float x = 0.f;
-    float y = 0.f;
-};
-
-class drawable {
-public:
-    vec2 position;
-    vec2 size; 
+    Vec2 position;
+    Vec2 size;
+    SDL_Color color = { 255, 255, 255, 255 };
 
     virtual void draw(){}
 protected:
@@ -44,10 +69,8 @@ protected:
     }
 };
 
-class square : public drawable{
+class Square : public Drawable{
 public:
-    SDL_Color color = { 0, 0, 0, 255 };
-
     void draw() override {
         auto destination = get_destination();
         SDL_SetRenderDrawColor(drawing::renderer, color.r, color.g, color.b, color.a);
@@ -55,38 +78,86 @@ public:
     }
 };
 
-class image : public drawable{
+class Image : public Drawable{
 public:
     SDL_Texture* texture;
 
     void draw () override {
         auto destination = get_destination();
+        SDL_SetTextureColorMod(texture, color.r, color.g, color.b);
+        SDL_SetTextureAlphaMod(texture, color.a);
         SDL_RenderCopyF(drawing::renderer, texture, NULL, &destination);
+    }
+};
+
+class Text : public Image {
+private:
+    TTF_Font* font;
+    std::string text;
+public:
+    void set_font(TTF_Font* font) {
+        this->font = font;
+
+        set_text(text);
+    }
+
+    void set_text(const std::string& text){
+        if(font == nullptr) {
+            std::cout << "TEXT ERROR: Font is null";
+            return;
+        }
+        SDL_Color white = {255, 255, 255, 255};
+        SDL_Surface *surf = TTF_RenderUTF8_Blended(font, text.c_str(), white);
+        if(surf == nullptr){
+            std::cout << "TEXT ERROR: " << TTF_GetError() << '\n';
+            return;
+        }
+        texture = SDL_CreateTextureFromSurface(drawing::renderer, surf);
+            if(texture == nullptr){
+            std::cout << "TEXT ERROR: " << TTF_GetError() << '\n';
+        }
     }
 };
 
 int main(int argc, char* argv[]){
     SDL_Init(SDL_INIT_EVERYTHING);
     IMG_Init(IMG_INIT_PNG);
+    TTF_Init();
+
+    SDL_SetHint(SDL_HINT_MOUSE_TOUCH_EVENTS, "1");
 
     drawing::window = SDL_CreateWindow("Ubuntu", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, NULL);
     drawing::renderer = SDL_CreateRenderer(drawing::window, -1, SDL_RENDERER_ACCELERATED);
 
-    square my_square;
+    Square my_square;
     my_square.size.x = 50;
     my_square.size.y = 50;
     my_square.color.r = 255;
 
-    image my_image;
+    Image my_image;
     my_image.size.x = 300;
     my_image.size.y = 200;
     my_image.texture = IMG_LoadTexture(drawing::renderer, "./my_image.png");
+
+    TTF_Font* font  = TTF_OpenFont("./BethEllen-Regular.ttf", 24);
+    if(font == nullptr){
+        std::cout << "FONT NOT LOADED: " << TTF_GetError() << '\n';
+    }
+
+    Text my_text;
+    my_text.size.x = 400;
+    my_text.size.y = 100;
+    my_text.set_font(font);
+    my_text.set_text("Olá mundo!");
+
+    std::unordered_map<SDL_FingerID, Drawable*> to_move;
     
     bool quit = false;
     while(!quit){
         drawing::clear();
         my_square.draw();
         my_image.draw();
+        my_text.draw();
         drawing::present();
 
         SDL_Event e;
@@ -104,6 +175,28 @@ int main(int argc, char* argv[]){
                             my_square.position.x -= 10.f;
                             break;
                     }
+                    break;
+                case SDL_FINGERDOWN:
+                    if(e.tfinger.x < 0.33f){//pega cubo
+                        to_move.insert_or_assign(e.tfinger.fingerId, &my_square);
+                    } 
+                    else if(e.tfinger.x < 0.66f){//pega imagem
+                        to_move.insert_or_assign(e.tfinger.fingerId, &my_image);
+                    }
+                    else{//pega texto
+                        to_move.insert_or_assign(e.tfinger.fingerId, &my_text);
+                    }
+                    break;
+                case SDL_FINGERMOTION:{
+                        auto it = to_move.find(e.tfinger.fingerId);
+                        if(it != to_move.end()){
+                            auto drawable = it->second;
+                            drawable->position = drawing::screen_point_to_world(drawing::touch_point_to_screen(e.tfinger.x, e.tfinger.y));
+                        }
+                    }
+                    break;
+                case SDL_FINGERUP:
+                    to_move.erase(e.tfinger.fingerId);
                     break;
             }
         }
